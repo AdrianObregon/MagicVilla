6edf0d307bda8627986fe3bdfbb2d58afba1d169
Adrian Obregon <badrian7898@hotmail.com>
2023-04-17 22:06:06 -0600

    Agregar campos villaDTO

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -9,8 +9,14 @@ namespace MagicVilla_VillaAPI.Models.Dto
         [MaxLength(30)]
         public string Name { get; set; }
 
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
+
         public int Occupancy { get; set; }
 
         public int Sqft { get; set; }
+        public string ImageUrl { get; set; }//cambio
+        public string Amenity { get; set; }
     }
 }
